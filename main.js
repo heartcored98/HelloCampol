838938be151c14acd196c1ed3975365b9ad1815f
righthan <hanuright@gmail.com>
2018-05-06 22:04:51 +0900

    Main page Compl.
    Main page and layout for the other pages are added.
    Should we add additional page to distinguish requests and comleted tasks?

---
+++ main.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+
+	$('#todo').click(function() {
+		window.location.href='request.html';
+	})
+
+	$('#notice').click(function() {
+		window.location.href='cardnews.html';
+	})
+	
+})
