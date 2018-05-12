8c4e29dd4354cf2ec3d35a376479b231af58ecdc
righthan <hanuright@gmail.com>
2018-05-13 00:51:26 +0900

    Main layout

---
+++ request.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+	$('#redir_request').click(function(){
+		window.location.href='request.html';
+	});
+	
+	$('#redir_notice').click(function(){
+		window.location.href='cardnews.html';
+	});
+	
+	$('#gohome').click(function(){
+		window.location.href='index.html';
+	})
+})
+
+
