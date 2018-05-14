0c490d0b5a0b36f38132694784d086e393abffc0
righthan <hanuright@gmail.com>
2018-05-14 14:33:24 +0900

    Change word

--- cardnews.js
+++ cardnews.js
@@ -41,4 +41,22 @@ $(document).ready(function () {
         window.location.href = 'index.html';
     })
 
+		// ====== Facebook Upload Setting ====== //
+	  window.fbAsyncInit = function() {
+			FB.init({
+				appId      : '192630588032448',
+				xfbml      : true,
+				version    : 'v3.0'
+			});
+			FB.AppEvents.logPageView();
+  	};
+
+		(function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) {return;}
+			 js = d.createElement(s); js.id = id;
+			 js.src = "https://connect.facebook.net/en_US/sdk.js";
+			 fjs.parentNode.insertBefore(js, fjs);
+		 }(document, 'script', 'facebook-jssdk'));
+	
 });
