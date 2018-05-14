4eedf5644cc7de274f5a7ec91330d346b6883b96
Hemistone <kei6040@gmail.com>
2018-05-14 09:29:23 +0900

    Minor Change on cardnews js (added goto 해야할 일 and home button)

--- cardnews.js
+++ cardnews.js
@@ -10,22 +10,35 @@ $(document).ready(function () {
         },
     });
 
-    
+    //====== Card News Loading Functions ======//
     $("#trafficimg").click(function () {
         window.location.href='cardnews_1.html';
-    })
+    });
 
     $("#dormitoryimg").click(function () {
         window.location.href='cardnews_2.html';
-    })
+    });
 
     $("#laboratoryimg").click(function () {
         window.location.href='cardnews_3.html';
-    })
+    });
 
     $("#drinkingimg").click(function () {
         window.location.href='cardnews_4.html';
+    });
+
+
+    // ====== Page Redirection Setting =======//
+    $('#redir_request').click(function () {
+        window.location.href = 'request.html';
+    });
+
+    $('#redir_notice').click(function () {
+        window.location.href = 'cardnews.html';
+    });
+
+    $('#gohome').click(function () {
+        window.location.href = 'index.html';
     })
 
-
-})
+});
