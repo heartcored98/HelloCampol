926d4bcb1a0b4014f629a5bd7db97e9bc61cfa5f
Hemistone <kei6040@gmail.com>
2018-05-17 03:37:58 +0900

    anime js 제외했음 granim.min.js로 동적 그라데이션을 추가해보았지만 보류중 main page padding 조절 완료 request에서 한번더 popup이 뜨도록 만듦.

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 $( document ).ready(function() {
-    anime({
-        targets: '#morphing .polymorph',
-        points: [
-            { value: '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369' },
-            { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
-            { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
-            { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
-        ],
-        easing: 'easeOutQuad',
-        duration: 2000,
-        loop: true
+
+	/* Code of Granim.js
+    var granimInstance = new Granim({
+        element: '#logo-canvas',
+        direction: 'left-right',
+        opacity: [1, 1],
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#6f97ff', '#56ccff'],
+                    ['#0a4692', '#03125c'],
+                    ['#497ed0', '#497ed0'],
+					['#00033b', '#130950']
+                ],
+                transitionSpeed: 7000
+            }
+        }
     });
+    granimInstance;
+    */
+
 	$('#todo').click(function() {
 		window.location.href='request.html';
 	})
